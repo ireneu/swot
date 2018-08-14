85f5542a93260e4ce3e22304a4a6d9feb5cbf755
Ireneu Pla <ireneup@gmail.com>
2018-08-14 19:34:01 +0200

    :white_check_mark: Add tests

--- Tests/SwotTests/SwotTests.swift
+++ Tests/SwotTests/SwotTests.swift
@@ -2,15 +2,104 @@ import XCTest
 @testable import Swot
 
 final class SwotTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Swot().text, "Hello, World!")
+    let jsonChangeset = """
+        {
+            "operations": [
+                {
+                    "type": "keep",
+                    "value": 5
+                },
+                {
+                    "type": "add",
+                    "value": "asdf"
+                },
+                {
+                    "type": "remove",
+                    "value": 3
+                },
+                {
+                    "type": "keep",
+                    "value": 4
+                },
+                {
+                    "type": "add",
+                    "value": "zxcv"
+                }
+            ]
+        }
+    """
+    let anotherChangeset = Changeset(operations: [
+        Remove(value: 1),
+        Keep(value: 2),
+        Add(value: " a"),
+        Keep(value: 1),
+        Add(value: "e "),
+        Keep(value: 3),
+        Remove(value: 5),
+        Add(value: "ty"),
+        Keep(value: 1),
+        Remove(value: 4)
+        ])
+    let yetAnotherChangeset = Changeset(operations: [
+        Remove(value: 3),
+        Add(value: " ab"),
+        Keep(value: 3),
+        Remove(value: 5),
+        Add(value: "ty"),
+        Keep(value: 5),
+        Remove(value: 1)
+        ])
+    let baseText = "qwerty poiu!"
+    let firstApply = "qwertasdfoiu!zxcv"
+    
+    func testChangesetIsCodable() {
+        let decoder = JSONDecoder()
+        let changeset = try! decoder.decode(Changeset.self, from: jsonChangeset.data(using: .utf8)!)
+
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(changeset)
+        let string = String(data: data, encoding: .utf8)!
+        
+        XCTAssert(string.components(separatedBy: .whitespacesAndNewlines).joined() == jsonChangeset.components(separatedBy: .whitespacesAndNewlines).joined())
+    }
+    
+    func testChangesetApply() {
+        let decoder = JSONDecoder()
+        let changeset = try! decoder.decode(Changeset.self, from: jsonChangeset.data(using: .utf8)!)
+        
+        let result = try! changeset.apply(to: baseText)
+        
+        XCTAssert(result == firstApply)
+    }
+    
+    func testChangesetChaining() {
+        let decoder = JSONDecoder()
+        let changeset = try! decoder.decode(Changeset.self, from: jsonChangeset.data(using: .utf8)!)
+        
+        let transformedText = try! changeset.apply(to: baseText)
+        let retransformedText = try! anotherChangeset.apply(to: transformedText)
+        
+        XCTAssert(retransformedText == "we are tasty!")
+        
+        let chained = try! changeset >>> anotherChangeset
+        let chainedApplyText = try! chained.apply(to: baseText)
+        
+        XCTAssert(chainedApplyText == "we are tasty!")
+    }
+    
+    func testChangesetBlending() {
+        let blended = try! anotherChangeset <~> yetAnotherChangeset
+        
+        let resa = try! blended.right.apply(to: anotherChangeset.apply(to: firstApply))
+        let resb = try! blended.left.apply(to: yetAnotherChangeset.apply(to: firstApply))
+        
+        XCTAssert(resa == resb)
     }
 
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testChangesetIsCodable", testChangesetIsCodable),
+        ("testChangesetApply", testChangesetApply),
+        ("testChangesetChaining", testChangesetChaining),
+        ("testChangesetBlending", testChangesetBlending),
     ]
 }
