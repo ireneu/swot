d93f0be04aa1eece496039614f26ee0b710b7d53
Atom <fromatom@gmail.com>
2021-08-06 00:00:43 +0900

    Add text case "testChangesetComposing" to check multi byte

--- Tests/SwotTests/SwotMultiByteCharTests.swift
+++ Tests/SwotTests/SwotMultiByteCharTests.swift
@@ -28,6 +28,19 @@ final class SwotMultiByteCharTests: XCTestCase {
             ]
         }
     """
+    let anotherChangeset = Changeset(operations: [
+        Keep(value: 8),
+        Remove(value: 1),
+        Keep(value: 2),
+        Add(value: " a"),
+        Keep(value: 1),
+        Add(value: "e "),
+        Keep(value: 3),
+        Remove(value: 5),
+        Add(value: "ty🍴"),
+        Keep(value: 1),
+        Remove(value: 4)
+    ])
 
     // 👨‍👩‍👧.length == 8
     let baseText = "👨‍👩‍👧qwerty poiu!"
@@ -42,7 +55,23 @@ final class SwotMultiByteCharTests: XCTestCase {
         XCTAssert(result == firstApply)
     }
 
+    func testChangesetComposing() {
+        let decoder = JSONDecoder()
+        let changeset = try! decoder.decode(Changeset.self, from: jsonChangeset.data(using: .utf8)!)
+
+        let transformedText = try! changeset.apply(to: baseText)
+        let retransformedText = try! anotherChangeset.apply(to: transformedText)
+
+        XCTAssert(retransformedText == "👨‍👩‍👧we are tasty🍴!")
+
+        let chained = try! changeset >>> anotherChangeset
+        let chainedApplyText = try! chained.apply(to: baseText)
+
+        XCTAssert(chainedApplyText == "👨‍👩‍👧we are tasty🍴!")
+    }
+
     static var allTests = [
         ("testChangesetApply", testChangesetApply),
+        ("testChangesetComposing", testChangesetComposing),
     ]
 }
